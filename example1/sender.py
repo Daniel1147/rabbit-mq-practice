b6dc7b2cd1ece6564ab6409b05a094dcbe443fc5
Daniel1147 <sxn91401@gmail.com>
2020-10-19 15:24:11 +0800

    add time into message of sender in example1

--- example1/sender.py
+++ example1/sender.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import pika
+from datetime import datetime
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
@@ -7,6 +8,7 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+message = '({}) Hello World!'.format(datetime.now().strftime("%H:%M:%S"))
+channel.basic_publish(exchange='', routing_key='hello', body=message)
+print(" [x] Sent '{}'".format(message))
 connection.close()
